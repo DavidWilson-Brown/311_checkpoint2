ee06dd0f138dad3365720bb6a509f0e6750fc902
David Wilson-Brown <wilsonbrowndavid@gmail.com>
2022-04-24 23:26:32 -0500

    Initial connection and creation of react app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 
 
 app.use(express.json());
+app.use(cors());
 
 
 let routes = require("./routes");
@@ -15,8 +17,15 @@ app.use(routes);
 let port = process.env.PORT || 8000;
 
 
-app.get("/", function(req, res){
-    res.send("Hello " + process.env.developer);
+// app.get("/", function(req, res){
+//     res.send("Hello " + process.env.developer);
+// });
+
+app.use(express.static(__dirname + "/../client/build"));
+
+
+app.get("/", function(_, res){
+    res.sendFile(__dirname + "/../client/build/index.html");
 });
 
 app.listen(port, function(){
